a66548101dc78a3850644a982a339e10653f4e2e
Wilson Dev <wilson.hch@hynkor.com>
2023-09-20 18:45:06 -0500

    Estados declarativos con useReducer

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,4 +1,104 @@
 
+import React from "react";
+
+const SECURITY_CODE = 'paradigma'
+
+function UseReducer({ name }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+
+  console.log(state)
+
+  // const onWrite = (newValue) => {
+  //   setState({
+  //     ...state,
+  //     value: newValue,
+  //   })
+  // }
+
+
+  React.useEffect(() => {
+    console.log("Empezando el efecto")
+
+    if (!!state.loading) {
+      setTimeout(() => {
+        console.log("Haciendo la velidación")
+
+        if (state.value === SECURITY_CODE) {
+          dispatch({ type: 'CONFIRM' })
+        } else {
+          dispatch({ type: 'ERROR' })
+        }
+        console.log("Terminando la validación")
+      }, 3000)
+    }
+
+    console.log("Terminando el efecto")
+  }, [state.loading])
+
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div >
+        <h2>Eliminar {name}</h2>
+        <p>Por favor, escribe el código de seguridad.</p>
+        {(state.error && !state.loading) && (
+          <p>Error: el código es incorrecto</p>
+        )}
+        {state.loading && (
+          <p>Cargando...</p>
+        )}
+        <input
+          placeholder="Código de seguridad"
+          value={state.value}
+          onChange={(event) => {
+            dispatch({ type: 'WRITE', payload: event.target.value })
+            // onWrite(event.target.value)
+          }} />
+        <button
+          onClick={() => {
+            dispatch({ type: 'CHECK' })
+            // onCheck()
+          }}>
+          Comprobar
+        </button>
+      </div>
+    )
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <>
+        <p>¿Seguro que quieres eliminar UseState?</p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'DELETE' })
+            // onDelete()
+          }}>
+          Sí, Eliminar
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'RESET' })
+            // onReset()
+          }}>
+          No, Volver
+        </button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <p>Eliminando con éxito</p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'RESET' })
+            // onReset()
+          }}
+        >
+          Resetear, volver atrás
+        </button>
+      </>
+    )
+  }
+}
+
 const initialState = {
   value: '',
   error: false,
@@ -7,64 +107,43 @@ const initialState = {
   confirmed: false,
 }
 
-// const reducer = (state, action) => {
-//   // null
-// }
-
-const reducerIf = (state, action) => {
-  if (action.type === 'ERROR') {
-    return {
-      ...state,
-      error: true,
-      loading: false,
-    }
-  } else if (action.type === 'CHECK') {
-    return {
-      ...state,
-      loading: true,
-    }
-  } else {
-    return {
-      ...initialState,
-    }
-  }
-}
-
-const reducerSwitch = (state, action) => {
-  switch (action.type) {
-    case 'ERROR':
-      return {
-        ...state,
-        error: true,
-        loading: false,
-      }
-    case 'CHECK':
-      return {
-        ...state,
-        loading: true,
-      }
-    default:
-      return {
-        ...state,
-      }
-  }
-}
-
-const reducerObject = (state) => ({
+const reducerObject = (state,payload) => ({
+  'CONFIRM': {
+    ...state,
+    error: false,
+    loading: false,
+    confirmed: true,
+  },
   'ERROR': {
     ...state,
     error: true,
     loading: false,
   },
+  'WRITE':{
+    ...state,
+    value: payload
+  },
   'CHECK': {
     ...state,
     loading: true,
+  },
+  'DELETE': {
+    ...state,
+    deleted: true,
+  },
+  'RESET': {
+    ...state,
+    confirmed: false,
+    deleted: false,
+    value: '',
   }
 })
 const reducer = (state, action) => {
   if (reducerObject(state)[action.type]) {
-    return reducerObject(state)[action.type]
+    return reducerObject(state,action.payload)[action.type]
   } else {
     return state;
   }
 }
+
+export { UseReducer }
